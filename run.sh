774a5e5247e3f8dc1e49c0313bfffb13dcca0aea
Edwino Stein <edwino.stein@gmail.com>
2015-12-15 21:45:48 -0400

    Implementando o run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,12 @@
 
 # ************** Variaveis globais **************
 
+dirBuild="./build/";
+compileTool="./tools/compileOSX.sh";
+compileInvokation="UDP/src/UdpInterface.cpp UDP/src/UdpClient.cpp UDP/src/UdpServer.cpp";
+clientSrc="client.cpp"
+serverSrc="server.cpp"
+mainProgram="";
 
 # ************** Programa principal **************
 
@@ -15,5 +21,44 @@ if [ ! -d "$dirBuild" ]; then
   mkdir "$dirBuild";
   echo " OK"
 fi
+
+case $1 in
+
+	"client")
+ 		mainProgram=$clientSrc;
+ 	;;
+
+	"client.cpp")
+ 		mainProgram=$clientSrc;
+ 	;;
+
+	"server")
+ 		mainProgram=$serverSrc;
+ 	;;
+
+	"server.cpp")
+ 		mainProgram=$serverSrc;
+ 	;;
+
+ 	*)
+ 		echo "Opções: server ou client."
+ 		exit 0
+ 	;;
+esac
+
+echo
+echo -e "\t   ######################################################"
+echo -e "\t   ################ Compilando Aplicativo ###############"
+echo -e "\t   ######################################################"
+echo
+chmod +x "$compileTool";
+bash -c "$compileTool $mainProgram $compileInvokation";
+
+if [ $? -ne 0 ];  then
+	echo
+	echo " ***** Algo errado ocorreu durante a compilação do aplicativo *****"
+	exit 1;
+fi
+echo
 
 exit 0;
